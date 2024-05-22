a2deb3a50593070fd3daa43dfc81c812b815b04a
tevn23 <mabulatt23@gmail.com>
2024-05-22 06:10:42 +0200

    Task 7

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 --Creates table in the current database
-CREATE TABLE IF NOT EXISTS mysql.first_table ( id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS mysql.first_table (id INT, name VARCHAR(256));
