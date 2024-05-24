3f720ca80c3ecb6c932b296cc041e22f2584303a
tevn23 <mabulatt23@gmail.com>
2024-05-24 03:11:16 +0200

    Task 9 fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a second table and adds multiple rows
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
