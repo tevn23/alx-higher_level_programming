b127ffc85b722190754ac0d97b30a24d2cf802e9
tevn23 <mabulatt23@gmail.com>
2024-06-20 02:27:44 +0200

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,16 +2,16 @@
 
 const args = process.argv.slice(2);
 
- function add (a, b) {
-    return (a + b);
- }
+function add (a, b) {
+  return (a + b);
+}
 
 const num1 = parseInt(args[0]);
 const num2 = parseInt(args[1]);
 
 if (!isNaN(num1) && !isNaN(num2)) {
-    const result = add(num1, num2);
-    console.log(result);
+  const result = add(num1, num2);
+  console.log(result);
 } else {
-    console.log('NaN')
+  console.log('NaN');
 }
