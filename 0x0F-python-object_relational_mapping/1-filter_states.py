6be05d72bc53af218ffd02ec7350f63b060df790
tevn23 <mabulatt23@gmail.com>
2024-06-22 16:37:01 +0200

    Task 1 fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,16 +6,16 @@ import MySQLdb
 
 def list_states_with_upper_N(username, password, database):
     """Lists all states with upper N from the database hbtn_0e_0_usa"""
-    if not username or not password or not database
+    if not username or not password or not database:
         sys.exit(1)
 
     db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-    )
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
 
     cursor = db.cursor()
     query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
