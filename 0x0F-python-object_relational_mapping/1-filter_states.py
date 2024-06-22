cda306e7fe70be7401c1fe8637f875f2bf0a3eb9
tevn23 <mabulatt23@gmail.com>
2024-06-22 16:11:13 +0200

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module containing function to list states"""
+import sys
+import MySQLdb
+
+
+def list_states_with_upper_N(username, password, database):
+    """Lists all states that starts with upper N from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./list_states.py <mysql username> <mysql password> "
+              "<database name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states_with_upper_N(username, password, database)
