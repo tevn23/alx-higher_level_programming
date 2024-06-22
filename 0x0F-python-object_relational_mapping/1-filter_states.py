3637c3e924f1979f8997813c64b1af622a317e32
tevn23 <mabulatt23@gmail.com>
2024-06-22 17:06:08 +0200

    Task 1 fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python3
-"""Module containing function to list states"""
+"""
+Module containing function to list states
+"""
 import sys
 import MySQLdb
 
 
 def list_states_with_upper_N(username, password, database):
-    """Lists all states with upper N from the database hbtn_0e_0_usa"""
-    if not username or not password or not database:
-        sys.exit(1)
+    """
+    Lists all states with upper N from the database hbtn_0e_0_usa
+    """
+    try:
+        if not username or not password or not database:
+            sys.exit(1)
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
         )
 
-    cursor = db.cursor()
-    query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-    states = cursor.fetchall()
+        cursor = db.cursor()
+        query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cursor.execute(query)
+        states = cursor.fetchall()
 
-    for state in states:
-        print(state)
+        if not states:
+            sys.exit(0)
+        
+        for state in states:
+            print(state)
 
-    cursor.close()
-    db.close()
-
+    except MySQLdb.Error as e:
+        sys.exit(1)
+    
+    except Exception as e:
+        sys.exit(1)
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
