e856548a8347930e9449e1d2d0ebcdb5f72e0366
tevn23 <mabulatt23@gmail.com>
2024-06-23 17:09:41 +0200

    Task 2 amendments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,10 +2,10 @@
 import sys
 import MySQLdb
 
+
 def main():
-    # Ensure the script receives exactly 4 arguments (excluding the script name)
+    """Display state table values """
     if len(sys.argv) != 5:
-        print("Usage: ./<script_name> <mysql_username> <mysql_password> <database_name> <state_name>")
         return
 
     mysql_username = sys.argv[1]
@@ -14,7 +14,6 @@ def main():
     state_name = sys.argv[4]
 
     try:
-        # Connect to the MySQL database
         db = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -23,27 +22,20 @@ def main():
             db=database_name
         )
 
-        # Create a cursor object to interact with the database
         cur = db.cursor()
-
-        # Execute the SQL query
         query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
         cur.execute(query, (state_name,))
-
-        # Fetch all the rows from the executed query
         rows = cur.fetchall()
 
-        # Print the rows
         for row in rows:
             print(row)
 
-        # Close the cursor and database connection
         cur.close()
         db.close()
-    
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
+
+    except MySQLdb.Error:
         return
+
 
 if __name__ == "__main__":
     main()
