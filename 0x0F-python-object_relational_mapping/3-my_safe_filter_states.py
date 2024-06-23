a76c39473a8e13bd58b286d746cabffc6cfb67a2
tevn23 <mabulatt23@gmail.com>
2024-06-23 19:23:51 +0200

    Handled Possible SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+A script that takes in safe arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+
+Usage:
+./<script_name> <mysql_username> <mysql_password> <database_name> <state_name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+        c = db.cursor()
+        query = ("SELECT * FROM `states` WHERE BINARY `name` = %s "
+                 "ORDER BY `id` ASC")
+        c.execute(query, (state_name,))
+        [print(state) for state in c.fetchall()]
+    except MySQLdb.Error as e:
+        print("Error {}: {}".format(e.args[0], e.args[1]))
+    finally:
+        if c:
+            c.close()
+        if db:
+            db.close()
