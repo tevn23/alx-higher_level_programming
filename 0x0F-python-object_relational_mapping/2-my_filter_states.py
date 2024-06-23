4b688064dc6be9a6df5cd9ea233b1e22b92ce1d1
tevn23 <mabulatt23@gmail.com>
2024-06-23 17:48:20 +0200

    Task 2 amendments 1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+A script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+
+Usage:
+./<script_name> <mysql_username> <mysql_password> <database_name> <state_name>
+"""
+
 import sys
 import MySQLdb
 
