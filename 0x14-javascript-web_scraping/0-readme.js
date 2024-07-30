58f06a385a9e303f5467b1dbef444c57d7e7bb30
tevn23 <mabulatt23@gmail.com>
2024-07-31 01:57:54 +0200

    Documented the script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
