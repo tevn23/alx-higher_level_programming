32d25045d4b18bfa921490def4fa474ddfbd0c29
tevn23 <mabulatt23@gmail.com>
2024-07-31 01:50:50 +0200

    Implemented a reading js script

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
