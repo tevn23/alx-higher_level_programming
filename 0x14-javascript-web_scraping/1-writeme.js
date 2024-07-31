a3312ac10f02931bb7f7c92026623debd4a046ab
tevn23 <mabulatt23@gmail.com>
2024-07-31 02:07:23 +0200

    Added tha script that reads from a string

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+// Get the file path and string to write from the command line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
