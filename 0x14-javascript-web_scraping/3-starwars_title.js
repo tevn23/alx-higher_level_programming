104dde7af627901d765dd25d085647dc4092fa6c
tevn23 <mabulatt23@gmail.com>
2024-07-31 02:28:26 +0200

    Used SW api to get the name of the values

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Construct the API URL
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+     console.log(error);
+    } else if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+       console.log(data.title);
+      } else {
+        console.log(`Error: ${response.statusCode}`);
+      }
+});
